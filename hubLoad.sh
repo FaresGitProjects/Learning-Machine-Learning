5757558d01f1ee7687dbe2ff3afb37cae98abd69
Fares Mohamed <contact.fares.mohamed@gmail.com>
2023-01-24 07:52:01 -0500

    Auto Commit

--- hubLoad.sh
+++ hubLoad.sh
@@ -5,7 +5,7 @@ git remote remove origin
 git init
 git add -A
 git commit -m "Auto Commit"
-git remote add origin $1
+git remote add origin git@github.com:FaresGitProjects/"$PWD".git
 git branch -M main
 git push -u origin main
 
